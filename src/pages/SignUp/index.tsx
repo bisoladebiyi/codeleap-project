abc9ec2043992a05f09f74c13d36193dfa66d4fc
Abisola Adebiyi <abisola.ca@gmail.com>
2023-04-07 21:13:18 +0100

    include welcome page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Elements/Button";
 import Field from "../../components/Elements/Field";
+import WelcomePage from "../../components/WelcomePage";
 import { updateUserInfo } from "../../redux/features/userInfoSlice";
 import { colors } from "../../utils/constants/colors";
 import { routes } from "../../utils/constants/routes";
@@ -10,8 +11,10 @@ import "./SignUp.scss";
 
 const SignUp = () => {
   const [value, setValue] = useState<string>("");
+  const [showWelcomeScreen, setShowWelcomeScreen] = useState<boolean>(true);
 
   const username = useSelector((state: any) => state.user.username);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -20,6 +23,16 @@ const SignUp = () => {
       navigate(routes.HOME);
     }
   }, [username]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcomeScreen(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   const updateVal = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -34,7 +47,9 @@ const SignUp = () => {
     navigate(routes.HOME);
   };
 
-  return (
+  return showWelcomeScreen ? (
+    <WelcomePage />
+  ) : (
     <div className="SignUp">
       <div className="SignUp-Card">
         <h2 className="SignUp-Heading">Welcome to CodeLeap network!</h2>
